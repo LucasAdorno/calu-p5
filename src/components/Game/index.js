7b5cb8cf0210fc51136ff4547c3aeae113a6e1ac
LucasAdorno <lucasadorno2702@gmail.com>
2021-03-23 13:13:00 -0300

    COD: Removendo erro p5sound

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import P5Wrapper from 'react-p5-wrapper';
-import sketch from '../../sketches/sketch';
+import React from "react";
+import P5Sketch from "../../sketches/sketch";
 
 const Game = () => {
-
-  return(
-      <div >
-        <P5Wrapper sketch={sketch}></P5Wrapper>
-      </div>
-    );
-}
+  return (
+    <div>
+      <P5Sketch />
+    </div>
+  );
+};
 
 export default Game;
